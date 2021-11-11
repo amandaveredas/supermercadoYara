312a2e3d29d2d46cb3f5b87189d756e135c19592
amandaveredas <amandaveredas@gmail.com>
2021-11-11 19:34:11 -0300

    FEAT: Lista produtos
    Lista todos os produtos, devolvendo no formato ProdutoResponse

--- src/main/java/com/winningwomen/supermercadoYara/dto/response/ProdutoResponse.java
+++ src/main/java/com/winningwomen/supermercadoYara/dto/response/ProdutoResponse.java
@@ -1,16 +1,22 @@
 package com.winningwomen.supermercadoYara.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProdutoResponse {
 
 	private String nome;
 	private int quantidade;
-	private BigDecimal preco;
-	private long idCategoria;
+	private BigDecimal precoUnitario;
+	private String nomeCategoria;
 	private String descricao;
 	private String imagem;
 }
