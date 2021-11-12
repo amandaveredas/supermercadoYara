b2ba0ba3362f7176da2f40086b313c518413fb0c
Amanda Pereira <amanda.l.a.pereira@gmail.com>
2021-11-12 16:15:51 -0300

    POST GET PUT DELETE de usuario

--- src/main/java/com/winningwomen/supermercadoYara/controller/UsuarioController.java
+++ src/main/java/com/winningwomen/supermercadoYara/controller/UsuarioController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import com.winningwomen.supermercadoYara.model.Usuario;
 import com.winningwomen.supermercadoYara.repository.UsuarioRepository;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value="/usuario")
 public class UsuarioController {
@@ -13,8 +15,18 @@ public class UsuarioController {
 	@Autowired
 	UsuarioRepository usuarioRepository;
 	
-	@PostMapping
+	@PostMapping("")
 	public void post(@RequestBody Usuario usuario) {
 		usuarioRepository.save(usuario);
 	}
+
+	@GetMapping("")
+	public List<Usuario>ListaTodosUsuarios(){return usuarioRepository.findAll();}
+
+	@PutMapping("")
+	public Usuario atualizaUsuario(@RequestBody Usuario usuario){return usuarioRepository.save(usuario);}
+
+	@DeleteMapping("")
+	public void deletaUsuario(@RequestBody Usuario usuario){usuarioRepository.delete(usuario);}
+	
 }
